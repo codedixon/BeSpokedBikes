5e2b72fa1ba1f92abc6dd553e0003e69dc159922
codedixon <dixoncodes@gmail.com>
2021-01-27 13:13:04 -0500

    Add project files.

---
+++ Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeSpokedBikes.Models
+{
+    public class Customer
+    {
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeSpokedBikes.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Manufacturer { get; set; }
+        public string Style { get; set; }
+        public int PurchasePrice { get; set; }
+        public int SalePrice { get; set; }
+        public int Quantity { get; set; }
+        public decimal CommissionPercentage { get; set; }
+    }
+}
